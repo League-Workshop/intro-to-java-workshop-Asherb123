e06200fbba384467b1eabe2000297275e58f7854
League <league@WTS8.attlocal.net>
2019-01-02 13:26:31 -0800

    Day one.

--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -3,27 +3,29 @@ package section2;
 import org.jointheleague.graphical.robot.Robot;
 
 public class SmartShapes {
-	
-    public static void main(String[] args) throws Exception {
-        // 1. Make a new Robot
 
+	public static void main(String[] args) throws Exception {
+        // 1. Make a new Robot
+		Robot r2d2=new Robot();
 
         // 3. Put the robot's pen down
-
+		r2d2.penDown();
 
         // 6. Make the robot move as fast as possible
-
+		r2d2.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
+		for (int i = 0; i < 100000; i++) {
+	
 
 
         //         2. Move your robot 200 pixels
-
+		r2d2.move(100);
 
         //         4. Turn the robot 90 degrees to the right
-
-    	
+		r2d2.turn(90);
+    		
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
-   
+		}
     }
 }
