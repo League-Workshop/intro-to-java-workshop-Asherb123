254e1c5a553257004582101e97a7cdeb25a9cc09
League <league@WTS8.attlocal.net>
2019-01-03 13:23:12 -0800

    Day 2

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,29 +12,40 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int PlayerHealth=100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int DragonHealth=100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int PAttack=0;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int DAttack=0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(PlayerHealth>0 && DragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String S=JOptionPane.showInputDialog("Do you want to attack with a yell, or a kick?");
 		// 9. If they typed in "yell":
-		
+		if (S.equals("yell")){
+		int r=new Random().nextInt(10);
+		DAttack=r;
+		DragonHealth-=DAttack;
+		}
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable 
 			
 		// 10. If they typed in "kick":
-		
+		else {
+			int r=new Random().nextInt(10);
+			DAttack=r;
+			DragonHealth-=DAttack;	
+			
+			
+			
+		}
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable
@@ -60,6 +71,6 @@ public class DragonFight {
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
-		}
+		}}
 	}
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,32 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int Score=0;
 		// 2.  Ask the user a question 
-		
+		String s=JOptionPane.showInputDialog("Whats 1+1?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (s.equals("2")) {
+		Score++;
+		JOptionPane.showMessageDialog(null, "Correct!!!!!");
+		}
 		// 4.  if the user's answer is correct
 		
 		// -- add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String A=JOptionPane.showInputDialog("Whats 3X3?");
+		// 3.  Use an if statement to check if their answer is correct
+		if (A.equals("9")) {
+		Score++;
+		JOptionPane.showMessageDialog(null, "Correct!!!!!");
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, "Your score is "+ Score);
 	}
 }
